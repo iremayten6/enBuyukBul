8996abfdd11f76b4973c4237fd6c4cddae03bb09
İREM AYTEN <126502980+iremayten6@users.noreply.github.com>
2023-05-05 20:22:30 +0300

    Add project files.

---
+++ enBuyukBul/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace enBuyukBul
+{
+    partial class FormEnBüyükSayi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.txtSayi3 = new System.Windows.Forms.TextBox();
+            this.btnBul = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(94, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sayı 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(94, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 55);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Sayı 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(94, 267);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 55);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Sayı 3";
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(93, 471);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(415, 61);
+            this.lblSonuc.TabIndex = 0;
+            this.lblSonuc.Text = "En Büyük Sonuç";
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtSayi1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSayi1.Location = new System.Drawing.Point(300, 74);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(157, 62);
+            this.txtSayi1.TabIndex = 1;
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtSayi2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSayi2.Location = new System.Drawing.Point(300, 165);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(157, 62);
+            this.txtSayi2.TabIndex = 1;
+            // 
+            // txtSayi3
+            // 
+            this.txtSayi3.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtSayi3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSayi3.Location = new System.Drawing.Point(300, 260);
+            this.txtSayi3.Name = "txtSayi3";
+            this.txtSayi3.Size = new System.Drawing.Size(157, 62);
+            this.txtSayi3.TabIndex = 1;
+            // 
+            // btnBul
+            // 
+            this.btnBul.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnBul.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBul.Location = new System.Drawing.Point(176, 365);
+            this.btnBul.Name = "btnBul";
+            this.btnBul.Size = new System.Drawing.Size(220, 86);
+            this.btnBul.TabIndex = 2;
+            this.btnBul.Text = "Bul";
+            this.btnBul.UseVisualStyleBackColor = false;
+            this.btnBul.Click += new System.EventHandler(this.btnBul_Click);
+            // 
+            // FormEnBüyükSayi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(605, 621);
+            this.Controls.Add(this.btnBul);
+            this.Controls.Add(this.txtSayi3);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.txtSayi1);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormEnBüyükSayi";
+            this.Text = "En Büyük Sayıyı Hesapla";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.TextBox txtSayi3;
+        private System.Windows.Forms.Button btnBul;
+    }
+}
+
---
+++ enBuyukBul/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace enBuyukBul
+{
+    public partial class FormEnBüyükSayi : Form
+    {
+        public FormEnBüyükSayi()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBul_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2, sayi3;
+            sayi1 = Convert.ToInt32(txtSayi1.Text);
+            sayi2 = Convert.ToInt32(txtSayi2.Text);
+            sayi3 = Convert.ToInt32(txtSayi3.Text);
+
+            if (sayi1 >= sayi2 && sayi1 >= sayi3)
+            {
+                lblSonuc.Text = "En Büyük Sayı : " + sayi1;
+            }
+            else if (sayi2 > sayi1 && sayi2 > sayi3)
+            {
+                lblSonuc.Text = "En Büyük Sayı : " + sayi2;
+            }
+            else
+            {
+                lblSonuc.Text = "En Büyük Sayı : " + sayi3;
+            }
+        }
+    }
+}
